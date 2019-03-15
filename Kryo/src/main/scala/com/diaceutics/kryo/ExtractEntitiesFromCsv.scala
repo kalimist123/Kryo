fbeefbc19550af0dd5c92015e23381979dfdd778
JackTorrence <jacktorrence420@gmx.com>
2019-03-15 17:37:48 +0000

    Replace with UDF

--- Kryo/src/main/scala/com/diaceutics/kryo/ExtractEntitiesFromCsv.scala
+++ Kryo/src/main/scala/com/diaceutics/kryo/ExtractEntitiesFromCsv.scala
@@ -3,11 +3,11 @@ package com.diaceutics.kryo
 import org.clulab.processors.fastnlp.FastNLPProcessor
 import org.clulab.processors.{Document, Processor, Sentence}
 import org.apache.spark.sql.{SQLContext, SparkSession}
-
+import org.apache.spark.sql.functions.udf
 
 object ExtractEntitiesFromCsv {
 
-
+  val proc: Processor = new FastNLPProcessor()
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession
@@ -22,18 +22,25 @@ object ExtractEntitiesFromCsv {
     val empDF = spark.read.format("csv")
       .option("header", "true")
       .option("inferSchema", "true")
-      .load("Kryo\\src\\resources\\5c8ac617a7d1780004000081.csv")
+      .load("Kryo\\src\\resources\\1col.csv")
 
-    empDF.collect().foreach { x => {
-      println(x.getString(1))
-      val sentenceChunks = aggEnt.extractEntities(proc, x.getString(1))
-      sentenceChunks foreach { sentence =>
-        sentence foreach println
-      }
-
-    }
-    }
+    import spark.implicits._
+    val newDf = empDF.withColumn("annotated", lineAgg($"textstrings"))
+    newDf.show(1000, false)
 
   }
+
+
+  def lineAgg = udf((line: String) => {
+
+    val aggEnt = new NamedEntityAggregator();
+    val sentenceChunks = aggEnt.extractEntities(proc, line)
+    val sb = new StringBuilder
+    sentenceChunks foreach { sentence =>
+      sentence.map(s => sb.append(s.toString()))
+     }
+    sb.toString()
+  })
+
 
 }
